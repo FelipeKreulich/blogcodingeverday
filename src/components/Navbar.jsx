e9f48a8749b67680577150866786a3be9fbce13e
FelipeKreulich <felipek2002k@gmail.com>
2023-11-01 20:18:13 +0100

    Initial

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import logo from '../img/logoCE.png';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="container">
-        <div className="logo">Logo</div>
-        <div className="links">Links</div>
+        <div className="logo">
+          <img src={logo} alt="Coding Everday" />
+        </div>
+        <div className="links">
+          <Link className='link' to="/?cat=art">
+            <h6>ART</h6>
+          </Link>
+          <Link className='link' to="/?cat=art">
+            <h6>SCIENCE</h6>
+          </Link>
+          <Link className='link' to="/?cat=art">
+            <h6>TECHNOLOGY</h6>
+          </Link>
+          <Link className='link' to="/?cat=art">
+            <h6>CINEMA</h6>
+          </Link>
+          <Link className='link' to="/?cat=art">
+            <h6>DESIGN</h6>
+          </Link>
+          <Link className='link' to="/?cat=art">
+            <h6>FOOD</h6>
+          </Link>
+          <span>Kreulich</span>
+          <span>Logout</span>
+          <span className='write'>
+            <Link className='link' to="/write">Write</Link>
+          </span>
+        </div>
       </div>
     </div>
   )
