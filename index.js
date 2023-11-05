001a9c7bf6f6feed3b914ae67cf49c16629e57a0
FelipeKreulich <felipek2002k@gmail.com>
2023-11-05 17:53:45 +0100

    Create API

---
+++ index.js
@@ -0,0 +1,22 @@
+import express, { json } from "express";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}),json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+
+app.listen(8800, () => {
+  console.log("Conectado!");
+});
